c0d9dc983cba666f8888fde4a0dc317a6b261798
Rick Beyer <beyer.richard73@gmail.com>
2021-12-04 22:47:47 -0600

    Removed index.css from working

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -1,9 +1,16 @@
 import '../Styles/CurrentWeather.css';
+import Temperature from './Temperature';
 
 const CurrentWeather = () => {
     return (
         <div className='current-weather__container'>
-            
+            <p>RIGHT NOW</p>
+            <div className='current-weather__info'>
+                <Temperature />
+            </div>
+            <div className='current-weather__icon'>
+
+            </div>
         </div>
     )
 }
---
+++ src/Components/Temperature.js
@@ -0,0 +1,11 @@
+import '../Styles/Temperature.css';
+
+const Temperature = () => {
+    return (
+        <div className='temperature__container'>
+            60°<span>F</span>
+        </div>
+    )
+}
+
+export default Temperature;
