78d268083e92018bb9f2fb59a52c87908e5e20e2
Rick Beyer <beyer.richard73@gmail.com>
2021-12-04 23:05:56 -0600

    Temperature component done.

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -5,11 +5,14 @@ const CurrentWeather = () => {
     return (
         <div className='current-weather__container'>
             <p>RIGHT NOW</p>
-            <div className='current-weather__info'>
-                <Temperature />
-            </div>
-            <div className='current-weather__icon'>
-
+            <div className='current-weather__details'>
+                <div className='current-weather__info'>
+                    <Temperature />
+                    <p className='weather-type'>Light Thunderstorms</p>
+                </div>
+                <div className='current-weather__icon'>
+                    
+                </div>
             </div>
         </div>
     )
