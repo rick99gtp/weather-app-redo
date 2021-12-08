c3236f615e648cc34a1cd7c653d8516ed4776b44
Rick Beyer <beyer.richard73@gmail.com>
2021-12-08 09:06:20 -0600

    Added 10-day forecast. Need to style.

--- src/Components/TenDay.js
+++ src/Components/TenDay.js
@@ -1,7 +1,18 @@
 import '../Styles/TenDay.css';
 import React from 'react';
+import { useState, useContext } from 'react';
+import { WeatherContext } from '../WeatherContext';
 
 const TenDay = () => {
+    const data = useContext(WeatherContext);
+    const [ prefix ] = useState('https://weatherbit.io/static/img/icons/');
+
+    const getDay = (date) => {
+        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        const newDate = new Date(date);
+        const day = newDate.getDay();
+        return (days[day]);
+    };
 
     return (
         <div className='ten-day__container'>
@@ -11,17 +22,19 @@ const TenDay = () => {
                 <div>LOW</div>
                 <div>HIGH</div>
             </div>
-            {/* {context ? context.data.map(() => {
+
+            {data.data.map((el, idx) => {
+                console.log(el.datetime);
                 return (
-                    <div></div>
+                    <div className='ten-day__row' key={el.datetime}>
+                        <div className='col1'>{idx === 0 ? 'Today' : getDay(el.datetime)}</div>
+                        <div className='col2'><img src={`${prefix}${el.weather.icon}.png`} alt="daily-icon"/></div>
+                        <div className='col3'>{Math.floor(el.low_temp)}</div>
+                        <div className='col4'>{Math.floor(el.high_temp)}</div>
+                    </div>
                 )
-            }) : null} */}
-            <div className='ten-day__row'>
-                <div className='col1'>Today</div>
-                <div className='col2'>ICON</div>
-                <div className='col3'>50°</div>
-                <div className='col4'>60°</div>
-            </div>
+            })}
+
         </div>
     )
 }
